d7a999fdab7f5e72bf04b66e942970b90bc698a5
JessSm3 <93676850+JessSm3@users.noreply.github.com>
2023-02-02 16:42:26 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,18 @@ from PIL import Image
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)")
+st.write("[![Star](https://img.shields.io/github/stars/streamlit/streamlit.svg?logo=github&style=social)](https://gitHub.com/streamlit/streamlit)")
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('dp.png'))
+col2.image(Image.open('streamlitlogo.png'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('Streamlit')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('A faster way to build and share data and ML apps')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('blog', 'https://blog.streamlit.io', 'Streamlit blog', icon_size)
+st_button('youtube', 'https://www.youtube.com/@streamlit5916', 'Streamlit YouTube', icon_size)
+st_button('twitter', 'https://twitter.com/streamlit/', 'Follow on Twitter', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/company/streamlit/', 'Follow on LinkedIn', icon_size)
